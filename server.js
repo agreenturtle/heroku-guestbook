b5752d4acc909c824075b9f639a95ed34c5837db
James Chen <james.lai.chen@gmail.com>
2014-02-05 18:13:54 -0800

    adding delete

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.set('view engine', 'jade');
 
 app.use(express.static(process.cwd() + '/public'));
 
-//Counter for the ID index
-var indexCounter = 1; 
-
-/***************************************************************************/
+/*********************** Helper Functions *************************************/
 /**
 * This function will get the current date and time and return in the format 
   dd/mm/yyyy hh:mm:ss"
@@ -34,60 +31,32 @@ function GetDateTime(){
 	return datetime;
 }
 
-/**
-* This function will insert a row of data in the table "entries"
-* It will also calculate how many guests are entered in to correctly 
-  enter the next guest with the correct ID
-* guestName - is the name that the user entered
-* guestComment - is the comment that the user entered
-* dataRow - Array with the format of [ID#, name, comments, date/time]
-**/
-function InsertData(guestName,guestComment){
-	connection.query('SELECT count(id) AS totalIds FROM entries', function(err,rows){
-		console.log(rows);
-		if (err){throw err;} //it throws an error
-		else{
-			var dateTime = GetDateTime();	
-			var idCount = parseInt(rows[0].totalIds) + 1;
-			var dataRow = [rows[0].totalIds, guestName, guestComment, dateTime];			
-			/*var myString = 'INSERT INTO entries (id, name, comment, created_at) VALUES (\"'
-							 + dataRow[0] + '\", \"' 
-							 + dataRow[1] + '\", \"'
-				 			 + dataRow[2] + '\", STR_TO_DATE(\"' 
-							 + dataRow[3] + '\", \"%m/%d/%Y %H:%i:%s\"))';
-			console.log(myString);*/
-			
-			connection.query('INSERT INTO entries (id, name, comment, created_at) VALUES (\"'
-							 + dataRow[0] + '\", \"' 
-							 + dataRow[1] + '\", \"'
-				 			 + dataRow[2] + '\", STR_TO_DATE(\"' 
-							 + dataRow[3] + '\", \"%m/%d/%Y %H:%i:%s\"))');
-		}	
-	});
-}
+/*********************** Requests & Response *********************************/
 
 /**
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255), 
-  comment TEXT NOT NULL, 
-  created_at DATETIME NOT NULL, 
-  PRIMARY KEY(id))
+* From the start page will first open application.jade->application.html
+* When Submit Guest button is clicked it will enter data into the MySQL database, entries
+* dataRow - is the row of data that will be entered in to entries in the format:
+			[ID, name, comment, date]
 **/
-
-/**************************************************************************/
-
 app.get("/", function(request,response){
 	if(request.query.userComment && request.query.userComment != ''){ //check for page's initial load
-		//Insert data into the database
-		var guestName = request.query.userName;
-		var guestComment = request.query.userComment;
-		InsertData(guestName, guestComment);
-		
-		//ResultPage to pull the results from the database
-		connection.query('SELECT * FROM entries', function(err,rows){
-			//console.log(rows);
-			response.render('resultpage', {'rows':rows}); 
-			response.end();
+		connection.query('SELECT count(id) AS totalIds FROM entries', function(err,rows){
+
+			if (err){throw err;} 
+			else{
+				var dateTime = GetDateTime();	
+				var idCount = parseInt(rows[0].totalIds) + 1;
+				var dataRow = [rows[0].totalIds, request.query.userName, request.query.userComment, dateTime];			
+			
+				connection.query('INSERT INTO entries (id, name, comment, created_at) VALUES (\"'
+								 + dataRow[0] + '\", \"' 
+								 + dataRow[1] + '\", \"'
+					 			 + dataRow[2] + '\", STR_TO_DATE(\"' 
+								 + dataRow[3] + '\", \"%m/%d/%Y %H:%i:%s\"))');
+			}
+			response.render('application');
+			response.end();	
 		});
 	}
 	else{
@@ -96,14 +65,50 @@ app.get("/", function(request,response){
 	}
 });
 
-app.post("/", function(request,respond){
+/**
+* This is when the View Guestbook button is clicked (TO DO: Needs to enter comment to work)
+* Will render resultpage.jade and will display the table entries
+**/
+app.post("/ViewGuestbook", function(request,response){
 	connection.query('SELECT * FROM entries', function(err,rows){
-
 		response.render('resultpage', {'rows':rows}); 
 		response.end();
 	});
 });
 
+/**
+* Goes back to the HomePage (application.html) page
+**/
+app.post("/home", function(request,response){
+	response.render('application');
+	response.end();
+});
+
+app.post("/Delete", function(request,reponse){
+	response.render('deleteguest');
+	response.end();
+});
+
+/**
+* Deletes selected guest from entries
+**/
+app.post("/DeleteGuest", function(request,response){
+	if(request.query.userName == '' && request.query.userID = ''){
+		response.render('application');
+		response.end();
+	}
+	else{
+		connection.query('DELETE FROM entries WHERE id = ' + request.query.userID + 'AND name = ' + request.query.userName, function(err,rows){
+			if(err){throw err;}
+			else{
+				response.render('resultpage', {'rows':rows});
+				response.end();
+			}
+		});
+	}
+});
+
+/********************************************************************************/
 
 var port = Number(process.env.PORT || 8888);
 app.listen(port);
